4e28322cd5d6bc3f70b73e9b209d851c033a9333
Renato Oliveira <oliversieto@gmail.com>
2021-03-14 20:26:27 -0400

    refactor: adição de animações

--- lib/widgets/product/product-card.widget.dart
+++ lib/widgets/product/product-card.widget.dart
@@ -38,11 +38,14 @@ class ProductCard extends StatelessWidget {
                 ),
               );
             },
-            child: Image.asset(
-              image,
-              width: 170,
-              height: 170,
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: image,
+              child: Image.asset(
+                image,
+                width: 170,
+                height: 170,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Container(
